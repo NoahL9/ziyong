b4d06be1d045aae85b282400e8dcae172b0c2596
Noah <62173742+NoahL9@users.noreply.github.com>
2025-12-11 09:59:43 +0800

    Update and rename elabook.js to elab.js

---
+++ elab.js
@@ -0,0 +1,22 @@
+/***********************************
+
+> 應用名稱：咿啦看書
+> 軟件版本：2.8.5
+> 更新時間：2022-02-17
+
+ 
+[rewrite_local]
+
+# 咿啦看書
+https?:\/\/bookapi\.ellabook\.cn\/rest\/api\/service$ url script-response-body https://raw.githubusercontent.com/NoahL9/ziyong/refs/heads/main/elabook.js
+
+[mitm] 
+
+hostname=bookapi.ellabook.cn
+
+***********************************/
+body = $response.body.replace(/ip":"NO"/g, "ip\":\"YES\"")
+                     .replace(/ip":"VIP_NO"/g, "ip\":\"VIP_YES\"")
+                     .replace(/code":"\d{10}/g, "code\":\"0x00000000")
+                     .replace(/status":"\d/g, "status\":\"1");
+$done({body});
